1655052b3e1f8b5ec48f49bc7ee499a59553377b
Fon Godwill Ivan Tana <ivan8tana@gmail.com>
2024-07-22 10:04:46 +0100

    chore: Load environment variables before initializing the database

--- initialize.py
+++ initialize.py
@@ -1,10 +1,14 @@
 from config.mongodb import init_db
+from dotenv import load_dotenv
 import asyncio
 
 async def main():
+    print("Loading environment variables...")
+    load_dotenv()
     print("Starting database initialization...")
     await init_db()
     print("Database initialized")
 
 if __name__ == '__main__':
     asyncio.run(main())
+
